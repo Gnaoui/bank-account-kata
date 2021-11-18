968d939bfe555de1b1c85fc3061476eb97f0f9a6
Gnaoui Khriji <gnaoui.kheriji@gmail.com>
2021-11-18 23:51:21 +0100

    init test for Hostory PrintStatement

--- src/main/java/service/StatementPrinter.java
+++ src/main/java/service/StatementPrinter.java
@@ -1,5 +1,36 @@
 package service;
 
+import model.AccountStatement;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class StatementPrinter {
 
+    public void print(List<AccountStatement> accountStatementList) {
+        printHeader();
+        printStatementLinesFor(accountStatementList);
+    }
+
+    private void printHeader() {
+    }
+
+    private void printStatementLinesFor(List<AccountStatement> accountStatementList) {
+    }
+
+    private List<String> statementLines(List<AccountStatement> accountStatementList) {
+        return null;
+    }
+
+    private String statementLine(AtomicInteger runningBalance, AccountStatement t) {
+        return "";
+    }
+
+    private String formatDecimal(int amount) {
+        return "";
+    }
+
+    public void printLine(String line) {
+       // System.out.println(line);
+    }
 }
---
+++ src/test/java/service/PrintStatementFeature.java
@@ -0,0 +1,46 @@
+package service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PrintStatementFeature {
+
+    @Mock
+    DateFormatter dateFormatter;
+
+    @Mock
+    StatementPrinter statementPrinter;
+
+    private AccountTransactions accountTransactions;
+
+    @Before
+    public void initialise() {
+        Transactions transactions = new Transactions();
+        StatementPrinter statementPrinter = new StatementPrinter();
+        accountTransactions = new AccountTransactions(dateFormatter, transactions, statementPrinter);
+    }
+
+    @Test
+    public void should_print_statement_containing_all_transactions() {
+        BDDMockito.given(dateFormatter.dateAsString()).willReturn("2021-11-10 10:30");
+        accountTransactions.deposit(1000);
+        accountTransactions.withdrawal(100);
+        accountTransactions.deposit(500);
+    }
+
+    @Test
+    public void print_line_test() {
+        statementPrinter.printLine("Operation | Date             | Amount | Balance");
+        statementPrinter.printLine("Deposit | 2021-11-18 23:30 | 500,00 | 1400,00");
+        statementPrinter.printLine("Withdraw | 2021-11-18 18:30 | -100,00 | 900,00");
+        statementPrinter.printLine("Deposit | 2021-11-16 18:30 | 1000,00 | 1000,00");
+
+    }
+}
